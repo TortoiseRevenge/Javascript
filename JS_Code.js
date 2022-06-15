d28d5dba830cd9a2a342aa8b019dc886f2095a27
Adithya <asunderesan2020@gmail.com>
2022-06-15 13:51:19 -0500

    js

--- JS_Code.js
+++ JS_Code.js
@@ -1,2 +1,6 @@
-answer = prompt("Please enter your name.");
-console.log("Your name is " + answer);
+let length = prompt("What is the length of the box?");
+let width = prompt("What is the width of the box?");
+let height = prompt("What is the heightof this box?");
+
+let volume = width * length * height;
+console.log("The volume is " + volume);
