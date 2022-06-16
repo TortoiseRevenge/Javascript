3d9c8f54f1856eba9ed5cd4ee7d32c0d39eba35f
SeanR <s.m.reagan22@gmail.com>
2022-06-16 10:08:38 -0500

    NamesV2 but a little scuffed

--- Names.js
+++ Names.js
@@ -1,30 +1,25 @@
 let numOfNames = parseInt(prompt("how many friends do you have"));
 if (numOfNames>0){
-    let firstNames = [];
-    let lastNames = [];
-    let tempName = [];
+    let names = [];
     for (let i = 0;i<numOfNames;i++){
-        let tempName = (prompt("enter first name of person " + (i+1)).split(" "));
-        firstNames.push(tempName[0]);
-        lastNames.push(tempName[1])
+        names.push((prompt("enter first name of person " + (i+1))));
     }
+    function longestName(array){
     let longestLength = 0;
     let shortestLength = 999;
     let longestIndex = 0;
-    let shortestIndex = 0;
-    for(let j = 0; j<numOfNames;j++){
-        if(lastNames[j].length>longestLength){
-            longestLength = lastNames[j].length;
+    for(let j = 0; j<array.length;j++){
+        if(array[j].split(" ")[1].length>longestLength){
+            longestLength = array[j].split(" ")[1].length;
             longestIndex = j;
         }
-        if(lastNames[j].length<shortestLength){
-            shortestLength = lastNames[j].length;
-            shortestIndex = j;
+        if(array[j].split(" ")[1].length<shortestLength){
+            shortestLength = array[j].split(" ")[1].length;
         }
     }
-    console.log("person with longest last name: " + firstNames[longestIndex] + " " + lastNames[longestIndex]);
-    console.log("the range of last name lengths was: " + (longestLength-shortestLength));
-    for(let n = 0;n<numOfNames;n++){
-        console.log(firstNames[n] + " " + lastNames[n]);
+    return [array[longestIndex],(longestLength-shortestLength)]
     }
+    results = longestName(names);
+    console.log("Person with the longest name: " + results[0]);
+    console.log("Range of last name lengths: " + results[1]);
 }
