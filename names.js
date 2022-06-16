3d7de17a73f1bb10d08b90f097dcac2eb79e08d6
Adithya <asunderesan2020@gmail.com>
2022-06-16 10:22:44 -0500

    no map

--- names.js
+++ names.js
@@ -5,10 +5,36 @@ for (let x = 0; x < size; x ++) {
     z = prompt("Enter the person's full name: ");
     names.push(z);
 }
-let big = 0;
-for (let y = 0; y < names.length; y ++) {
-    if (names[y].substring(" ").length > big) {
-        big = y;
+const name = {
+    longest : "",
+    shortest : "",
+    range : 0
+};
+function findLongest(array) {
+    let big = 0;
+    let longest = "";
+    for (let y = 0; y < array.length; y ++) {
+        if (array[y].substring(" ").length > longest.length) {
+            big = y;
+            longest = array[y].substring(" ");
+        }
+    }
+    console.log("The name with the longest last name is " + array[big]);
+}
+findLongest(names);
+
+let sm = 20;
+let bg = 0;
+for (let f = 0; f < names.length; f ++) {
+    if ((names[f].substring(" ").length - 1) <= sm) {
+        sm = names[f].substring(" ").length - 1;
+    }
+    if (names[f].substring(" ").length >= bg) {
+        bg = names[f].substring(" ").length - 1;
     }
 }
-console.log("The person with the longest last name was " + names[big])
+console.log("The range of last name size was " + sm + " to " + bg + " letters.");
+console.log("All names: ");
+for (let h of names) {
+    console.log(h);
+}
