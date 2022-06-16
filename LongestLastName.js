0ef12d7d7b8555abc16773dd97118bcde11f4cbf
Divya Krishnan <divya.brookfield@gmail.com>
2022-06-16 10:24:21 -0500

    function map

--- LongestLastName.js
+++ LongestLastName.js
@@ -6,8 +6,18 @@ for (let i = 0; i < num; i++) {
 }
 let length = 0,
   longest;
-arr.forEach((e) => {
+nameList.forEach((e) => {
   const len = e.split(" ")[1].length;
   if (len > length) (length = len), (longest = e);
 });
+let total = 0;
 console.log(longest);
+nameList.forEach((n) => {
+  total += nameList.n;
+});
+let avg = total / nameList.length;
+console.log(nameList);
+
+function lastNameLength(num) {
+  return num.split(" ")[1].length;
+}
